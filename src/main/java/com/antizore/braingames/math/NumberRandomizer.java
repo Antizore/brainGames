7ebd85bf533debb1b68af78c26490fe5cdb37026
Antizore <2906pascal@gmail.com>
2026-06-01 17:16:20 +0200

    adding new ways of generating equations. Making it more flexible

--- src/main/java/com/antizore/braingames/math/NumberRandomizer.java
+++ src/main/java/com/antizore/braingames/math/NumberRandomizer.java
@@ -1,4 +1,42 @@
 package com.antizore.braingames.math;
+import java.security.SecureRandom;
+
 
 public class NumberRandomizer {
+
+    public int[] generateNumbers(char[] operators){
+
+        SecureRandom secureRandom = new SecureRandom();
+        int numbersToGenerate = operators.length + 1;
+        int[] numberArrays = new int[numbersToGenerate];
+        int lowerBond = 1;
+        int upperBond = 100;
+
+        for(int i = numbersToGenerate-1; i >= 0; i--){
+            if(i == numbersToGenerate - 1) {
+                numberArrays[i] = secureRandom.nextInt(lowerBond, upperBond);
+                continue;
+            }
+            if(operators[i] == '/') {
+                numberArrays[i] = generateNumberForDivision(numberArrays[i+1], secureRandom);
+                continue;
+            }
+            numberArrays[i] = secureRandom.nextInt(lowerBond, upperBond);
+        }
+
+        return numberArrays;
+    }
+
+    private int generateNumberForDivision(int previousNumber, SecureRandom secureRandom){
+        int lowerBound = 1;
+        int upperBound = 5;
+        int result = secureRandom.nextInt(lowerBound,upperBound);
+        return result * previousNumber;
+    }
+
+
+
+
+
+
 }
--- src/main/java/com/antizore/braingames/math/OperationRandomizer.java
+++ src/main/java/com/antizore/braingames/math/OperationRandomizer.java
@@ -1,4 +1,27 @@
 package com.antizore.braingames.math;
 
+import java.security.SecureRandom;
+
 public class OperationRandomizer {
+
+    private final char[] operators = {'+','-','/','*'};
+
+    public char[] generateOperators(){
+        int numberOfOperators = 2;
+        char[] generatedOperators = new char[numberOfOperators];
+
+        SecureRandom secureRandom = new SecureRandom();
+        int lowerBond = 0;
+        int upperBond = operators.length-1;
+
+        for(int i = 0; i<generatedOperators.length; i++){
+            generatedOperators[i] = operators[secureRandom.nextInt(lowerBond, upperBond)];
+        }
+
+        return generatedOperators;
+    }
+
+
+
+
 }
