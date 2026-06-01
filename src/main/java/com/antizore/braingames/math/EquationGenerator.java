7fae38bcaf5d7668c0350a85fa6099deefc839bf
Antizore <2906pascal@gmail.com>
2026-06-01 17:15:24 +0200

    adding new ways of generating equations. Making it more flexible

--- src/main/java/com/antizore/braingames/math/EquationGenerator.java
+++ src/main/java/com/antizore/braingames/math/EquationGenerator.java
@@ -1,20 +1,35 @@
 package com.antizore.braingames.math;
 
-public class EquationGenerator2 {
 
-    private int difficultyLvl;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 
-    void operationRandomizer(int difficultyLvl) {
+public class EquationGenerator {
 
+
+
+
+    public static StringBuilder equation(){
+        NumberRandomizer nbr = new NumberRandomizer();
+        OperationRandomizer opr = new OperationRandomizer();
+
+        char[] generatedOperators = opr.generateOperators();
+        int[] generatedNumbers = nbr.generateNumbers(generatedOperators);
+
+        StringBuilder equation = new StringBuilder();
+
+        for(int i = 0; i<generatedNumbers.length;i++){
+            equation.append(generatedNumbers[i]);
+            if(i < generatedOperators.length) {
+                equation.append(' ');
+                equation.append(generatedOperators[i]);
+                equation.append(' ');
+            }
+        }
+
+        return equation;
     }
 
-    void numberRandomizer(int difficultyLvl) {
-
-    }
-
-    public String buildEquation() {
-        return null;
-    }
 
 }
--- src/main/java/com/antizore/braingames/math/MathGameService.java
+++ src/main/java/com/antizore/braingames/math/MathGameService.java
@@ -1,8 +1,34 @@
 package com.antizore.braingames.math;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
 public class MathGameService {
 
+    public StringBuilder generateEquation(){
+
+        String taskId = String.valueOf(java.util.UUID.randomUUID());
+
+        StringRedisTemplate redisTemplate = new StringRedisTemplate();
+        redisTemplate.opsForValue().set(
+                redisTemplate.opsForValue()
+                        .set("task:" + taskId, "25", Duration.ofSeconds(15)));
+
+
+        StringBuilder equation = EquationGenerator.equation();
+
+        Expression e = new ExpressionBuilder(String.valueOf(equation)).build();
+
+        int resultOfEquation = (int) e.evaluate();
+
+        return equation;
+    }
 
 
 
